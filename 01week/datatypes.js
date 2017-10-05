a28ed942dfa7efce7e1b56751b7b3bd3c4706e0c
Andrew Thomas <athomas3@trinity.edu>
2017-10-04 20:51:55 -0500

    start of hw1

---
+++ 01week/datatypes.js
@@ -0,0 +1,34 @@
+// Write a JavaScript program to display the current day and time.
+// use function, declare a variaable as a new date method and return it.
+
+// Write a JavaScript program to convert a number to a string.
+// use string method to turn number into string
+
+// Write a JavaScript program to convert a string to the number.
+// use js method number to convert it
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Boolean
+// Null
+// Undefined
+// Number
+// NaN
+// String
+// function that evaluates with a conditional 6 times for each data type
+
+// Write a JavaScript program that adds 2 numbers together.
+// function that adds the arguments together
+
+// Write a JavaScript program that runs only when 2 things are true.
+// function that recieves two items, if those two things are true
+// methods: function, 2 arguments, if/then statements
+function evaluatesToTrue(arg1, arg2){
+  if (arg1 && arg2){
+    return "both are true"
+  }{
+    return "nope"
+  }
+}
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+// Write a JavaScript program that runs when both things are not true.
